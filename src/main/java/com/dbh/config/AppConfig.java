2412fca640bbe38074b65c799942131ec90467e1
MD. SAMIUL ARAFIN <sami.cse.1112@gmail.com>
2024-05-19 18:07:21 +0600

    converted jsp to thymeleaf

--- src/main/java/com/dbh/config/AppConfig.java
+++ src/main/java/com/dbh/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.dbh.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,19 +11,41 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 @ComponentScan(basePackages = "com.dbh")
 public class AppConfig implements WebMvcConfigurer {
 
+    private final ApplicationContext context;
+
     @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/pages/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver ivr = new ThymeleafViewResolver();
+        ivr.setTemplateEngine(templateEngine());
+        ivr.setOrder(0);
+        return ivr;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(context);
+        templateResolver.setPrefix("/pages/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
     }
 
     @Override
--- src/main/java/com/dbh/entity/Employee.java
+++ src/main/java/com/dbh/entity/Employee.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Set;
 
 @Data
 @Builder
@@ -30,29 +29,4 @@ public class Employee implements Serializable {
 
     @Column(name = "EMPLOYEE_PASSWORD")
     private String password;
-
-    /*@OneToMany(
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL
-    )
-    @JoinTable(
-            name = "EMPLOYEE_ADDRESS_MAPPING",
-            joinColumns = @JoinColumn(name = "EMPLOYEE_ID"),
-            inverseJoinColumns = @JoinColumn(name = "EMPLOYEE_ADDRESS_ID")
-    )
-    private Set<EmployeeAddress> addresses;*/
-
-   /* @OneToMany(
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL
-    )
-    @JoinColumn(name = "EMPLOYEE_ADDRESS_ID")
-    private Set<EmployeeAddress> addresses;*/
-
-    @OneToMany(
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL,
-            mappedBy = "employee"
-    )
-    private Set<EmployeeAddress> addresses;
 }
