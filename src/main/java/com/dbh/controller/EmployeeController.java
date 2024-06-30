20bc2047ab91f4c610ac43d9284c68269075f386
MD. SAMIUL ARAFIN <sami.cse.1112@gmail.com>
2024-06-30 20:44:56 +0900

    converted from blocking to non-blocking

--- src/main/java/com/dbh/controller/EmployeeController.java
+++ src/main/java/com/dbh/controller/EmployeeController.java
@@ -5,7 +5,7 @@ import com.dbh.dto.response.EmployeeResponse;
 import com.dbh.entity.Employee;
 import com.dbh.mapper.EmployeeMapper;
 import com.dbh.service.EmployeeService;
-import com.dbh.validation.EmployeeValidator;
+import com.dbh.utils.ResponseBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,8 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import reactor.core.publisher.Mono;
 
 import static com.dbh.exception.ApiError.fieldError;
 import static com.dbh.utils.ResponseBuilder.error;
@@ -36,52 +35,60 @@ public class EmployeeController {
 
     private final EmployeeMapper employeeMapper;
 
-    private final EmployeeValidator employeeValidator;
-
     @PostMapping
     @Operation(summary = "save an employee")
-    public ResponseEntity<JSONObject> save(@Valid @RequestBody EmployeeRequest request, BindingResult bindingResult) throws JsonProcessingException {
-        //ValidationUtils.invokeValidator(employeeValidator, request, bindingResult);
+    public Mono<ResponseEntity<JSONObject>> save(@Valid @RequestBody EmployeeRequest request, BindingResult bindingResult) {
+
         if (bindingResult.hasErrors()) {
-            return badRequest().body(error(fieldError(bindingResult)).getJson());
+            return Mono.just(badRequest().body(error(fieldError(bindingResult)).getJson()));
         }
         Employee employee = employeeMapper.save(request);
-        Employee employeeResponse = employeeService.save(employee);
-        EmployeeResponse response = employeeMapper.from(employeeResponse);
-        return new ResponseEntity<>(success(response).getJson(), HttpStatus.CREATED);
+        return employeeService.save(employee).map(employeeResponse -> {
+            EmployeeResponse response = employeeMapper.from(employeeResponse);
+            return new ResponseEntity<>(success(response).getJson(), HttpStatus.OK);
+        });
     }
 
     @PutMapping
-    public ResponseEntity<JSONObject> update(@Valid @RequestBody EmployeeRequest dto, BindingResult bindingResult) throws JsonProcessingException {
+    public Mono<ResponseEntity<JSONObject>> update(@Valid @RequestBody EmployeeRequest dto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return badRequest().body(error(fieldError(bindingResult)).getJson());
+            return Mono.just(badRequest().body(error(fieldError(bindingResult)).getJson()));
         }
-        Employee employee = employeeService.findById(dto.getId()).orElseThrow();
-        employeeMapper.update(employee, dto);
-        Employee employeeResponse = employeeService.update(employee);
-        EmployeeResponse response = employeeMapper.from(employeeResponse);
-        return new ResponseEntity<>(success((response)).getJson(), HttpStatus.OK);
+        return employeeService.findById(dto.getId()).flatMap(optionalEmployee -> {
+            if (optionalEmployee.isPresent()) {
+                Employee employee = optionalEmployee.get();
+                employeeMapper.update(employee, dto);
+                return employeeService.update(employee).map(employeeResponse -> {
+                    EmployeeResponse response = employeeMapper.from(employeeResponse);
+                    return new ResponseEntity<>(success(response).getJson(), HttpStatus.OK);
+                });
+            } else {
+                return Mono.just(new ResponseEntity<>(error("Employee not found").getJson(), HttpStatus.NOT_FOUND));
+            }
+        });
     }
 
     @GetMapping
     @Operation(summary = "get all  employees")
-    public ResponseEntity<JSONObject> findAll() throws JsonProcessingException {
-        List<Employee> employees = employeeService.findAll();
-        log.info("employees: {}", employees);
-        return new ResponseEntity<>(success(employees).getJson(), HttpStatus.OK);
+    public Mono<ResponseEntity<JSONObject>> findAll() {
+        return employeeService.findAll().collectList().map(employees -> {
+            log.info("employees: {}", employees);
+            return new ResponseEntity<>(success(employees).getJson(), HttpStatus.OK);
+        });
     }
 
     @GetMapping("{id}")
     @Operation(summary = "find an employee by id")
-    public ResponseEntity<JSONObject> findById(@PathVariable Long id) throws JsonProcessingException {
-        Employee employee = employeeService.findByEmployeeId(id);
-        return new ResponseEntity<>(success(employee).getJson(), HttpStatus.OK);
+    public Mono<ResponseEntity<JSONObject>> findById(@PathVariable Long id) {
+        return employeeService.findByEmployeeId(id)
+                .map(employee -> new ResponseEntity<>(ResponseBuilder.success(employee).getJson(), HttpStatus.OK))
+                .switchIfEmpty(Mono.just(new ResponseEntity<>(error("Employee nof dound ").getJson(), HttpStatus.NOT_FOUND)));
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "delete employee by id")
-    public ResponseEntity<JSONObject> delete(@PathVariable Long id) throws JsonProcessingException {
-        employeeService.delete(id);
-        return new ResponseEntity<>(success("employee deleted by id: " + id).getJson(), HttpStatus.OK);
+    public Mono<ResponseEntity<JSONObject>> delete(@PathVariable Long id) throws JsonProcessingException {
+        return employeeService.delete(id)
+                .thenReturn(new ResponseEntity<>(success("Employee deleted").getJson(), HttpStatus.OK));
     }
 }
--- src/main/java/com/dbh/service/EmployeeService.java
+++ src/main/java/com/dbh/service/EmployeeService.java
@@ -5,124 +5,123 @@ import com.dbh.repository.EmployeeRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-    private final EntityManager entityManager;
-
-    private final RestTemplate restTemplate;
-
     private final ObjectMapper objectMapper;
 
     @Transactional
-    public Employee save(Employee employee) throws JsonProcessingException {
+    public Mono<Employee> save(Employee employee) {
         String queryURL = "http://localhost:9999/dbh-command-api/dbh-employee-command";
         WebClient webClient = WebClient.builder().build();
-        Mono<String> response = webClient
+        return webClient
                 .post()
                 .uri(queryURL)
                 .bodyValue(employee)
                 .retrieve()
-                .bodyToMono(String.class);
-        String responseString = response.block();
-        return objectMapper.readValue(responseString, new TypeReference<>() {
+                .bodyToMono(String.class)
+                .map(responseString -> {
+                    try {
+                        return objectMapper.readValue(responseString, new TypeReference<>() {
+                        });
+                    } catch (JsonProcessingException ex) {
+                        throw new RuntimeException();
+                    }
         });
     }
 
     @Transactional
-    public Employee update(Employee employee) throws JsonProcessingException {
+    public Mono<Employee> update(Employee employee) {
         String queryURL = "http://localhost:9999/dbh-command-api/dbh-employee-command";
         WebClient webClient = WebClient.builder().build();
-        Mono<String> response = webClient
+        return webClient
                 .put()
                 .uri(queryURL)
                 .bodyValue(employee)
                 .retrieve()
-                .bodyToMono(String.class);
-        String responseString = response.block();
-        return objectMapper.readValue(responseString, new TypeReference<>() {
+                .bodyToMono(String.class)
+                .map(responseString -> {
+                    try {
+                        return objectMapper.readValue(responseString, new TypeReference<>() {
+                        });
+                    } catch (JsonProcessingException ex) {
+                        throw new RuntimeException();
+                    }
         });
     }
 
     @Transactional(readOnly = true)
-    public Optional<Employee> findById(Long id) {
-        return employeeRepository.findById(id);
+    public Mono<Optional<Employee>> findById(Long id) {
+        return Mono.fromSupplier(() -> employeeRepository.findById(id));
     }
 
     @Transactional
-    public void delete(Long id) throws JsonProcessingException {
+    public Mono<Void> delete(Long id) throws JsonProcessingException {
         String queryURL = "http://localhost:9999/dbh-command-api/dbh-employee-command/{id}";
         WebClient webClient = WebClient.builder().build();
-        Mono<String> response = webClient
+        return webClient
                 .delete()
                 .uri(queryURL, id)
                 .retrieve()
-                .bodyToMono(String.class);
-        String responseString = response.block();
-        objectMapper.readValue(responseString, new TypeReference<>() {
-        });
+                .bodyToMono(void.class);
     }
 
     @Transactional(readOnly = true)
-    public Optional<Employee> findByEmail(String email) {
-        return employeeRepository.findByEmail(email);
+    public Mono<Optional<Employee>> findByEmail(String email) {
+        return Mono.fromSupplier(() -> employeeRepository.findByEmail(email));
     }
 
     @Transactional(readOnly = true)
-    public Employee findByEmployeeId(Long id) throws JsonProcessingException {
+    public Mono<Employee> findByEmployeeId(Long id) {
         String queryURL = "http://localhost:9999/dbh-query-api/dbh-employee-query/{id}";
         WebClient webClient = WebClient.builder().build();
-        Mono<String> response = webClient
+        return webClient
                 .get()
                 .uri(queryURL, id)
                 .retrieve()
-                .bodyToMono(String.class);
-        String responseString = response.block();
-        return objectMapper.readValue(responseString, new TypeReference<>() {
+                .bodyToMono(String.class)
+                .map(responseString -> {
+                    try {
+                        return objectMapper.readValue(responseString, new TypeReference<>() {
+                        });
+                    } catch (JsonProcessingException ex) {
+                        throw new RuntimeException();
+                    }
         });
     }
 
-    //DTO based projection
-    /*@Transactional(readOnly = true)
-    public List<EmployeeProjection> findAll() {
-        return entityManager.createQuery("select " +
-                        "new com.dbh.dto.projection.EmployeeProjection(e.id, e.email) from Employee e",
-                EmployeeProjection.class).getResultList();
-    }*/
-
-    // Using RestTemplate
-    /*@Transactional(readOnly = true)
-    public List<Employee> findAll() throws JsonProcessingException {
-        String queryURL = "http://localhost:9999/dbh-query-api/dbh-employee-query";
-        ResponseEntity<String> response = restTemplate.getForEntity(queryURL, String.class);
-        return objectMapper.readValue(response.getBody(), new TypeReference<>() {});
-    }*/
-
     @Transactional(readOnly = true)
-    public List<Employee> findAll() throws JsonProcessingException {
+    public Flux<Employee> findAll() {
         String queryURL = "http://localhost:9999/dbh-query-api/dbh-employee-query";
         WebClient webClient = WebClient.builder().build();
-        Mono<String> response = webClient
+        return webClient
                 .get()
                 .uri(queryURL)
                 .retrieve()
-                .bodyToMono(String.class);
-        String responseString = response.block();
-        return objectMapper.readValue(responseString, new TypeReference<>() {
-        });
+                .bodyToMono(String.class)
+                .flatMapMany(responseString -> {
+                    try {
+                        List<Employee> employees = objectMapper.readValue(responseString, new TypeReference<>() {});
+                        return Flux.fromIterable(employees);
+                    } catch (JsonProcessingException ex) {
+                        throw new RuntimeException("Error deserializing response", ex);
+                    }
+                });
     }
+
 }
